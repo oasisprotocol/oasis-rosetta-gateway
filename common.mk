a22f8d4e8e831c129551dd416267f37209c91bf2
Tadej Janež <tadej.j@nez.si>
2020-10-21 16:49:58 +0200

    Make: Add common.mk, add lint targets, unify with other Oasis repos

---
+++ common.mk
@@ -0,0 +1,79 @@
+# Use Bash shell.
+# NOTE: You can control which Bash version is used by setting the PATH
+# appropriately.
+SHELL := bash
+
+# Check if we're running in an interactive terminal.
+ISATTY := $(shell [ -t 0 ] && echo 1)
+
+# If running interactively, use terminal colors.
+ifdef ISATTY
+	MAGENTA := \e[35;1m
+	CYAN := \e[36;1m
+	RED := \e[0;31m
+	OFF := \e[0m
+	ECHO_CMD := echo -e
+else
+	MAGENTA := ""
+	CYAN := ""
+	RED := ""
+	OFF := ""
+	ECHO_CMD := echo
+endif
+
+# Output messages to stderr instead stdout.
+ECHO := $(ECHO_CMD) 1>&2
+
+# Boolean indicating whether to assume the 'yes' answer when confirming actions.
+ASSUME_YES ?= 0
+
+# Helper that asks the user to confirm the action.
+define CONFIRM_ACTION =
+	if [[ $(ASSUME_YES) != 1 ]]; then \
+		$(ECHO) -n "Are you sure? [y/N] " && read ans && [ $${ans:-N} = y ]; \
+	fi
+endef
+
+# Name of git remote pointing to the canonical upstream git repository, i.e.
+# git@github.com:oasisprotocol/<repo-name>.git.
+GIT_ORIGIN_REMOTE ?= origin
+
+# Name of the branch where to tag the next release.
+RELEASE_BRANCH ?= master
+
+# Go binary to use for all Go commands.
+OASIS_GO ?= go
+
+# Go command prefix to use in all Go commands.
+GO := env -u GOPATH $(OASIS_GO)
+
+# Helper that ensures the git workspace is clean.
+define ENSURE_GIT_CLEAN =
+	if [[ ! -z `git status --porcelain` ]]; then \
+		$(ECHO) "$(RED)Error: Git workspace is dirty.$(OFF)"; \
+		exit 1; \
+	fi
+endef
+
+# Helper that checks if the go mod tidy command was run.
+# NOTE: go mod tidy doesn't implement a check mode yet.
+# For more details, see: https://github.com/golang/go/issues/27005.
+define CHECK_GO_MOD_TIDY =
+    $(GO) mod tidy; \
+    if [[ ! -z `git status --porcelain go.mod go.sum` ]]; then \
+        $(ECHO) "$(RED)Error: The following changes detected after running 'go mod tidy':$(OFF)"; \
+        git diff go.mod go.sum; \
+        exit 1; \
+    fi
+endef
+
+# Helper that checks commits with gitlilnt.
+# NOTE: gitlint internally uses git rev-list, where A..B is asymmetric
+# difference, which is kind of the opposite of how git diff interprets
+# A..B vs A...B.
+define CHECK_GITLINT =
+	BRANCH=$(GIT_ORIGIN_REMOTE)/$(RELEASE_BRANCH); \
+	COMMIT_SHA=`git rev-parse $$BRANCH` && \
+	$(ECHO) "$(CYAN)*** Running gitlint for commits from $$BRANCH ($${COMMIT_SHA:0:7})... $(OFF)"; \
+	gitlint --commits $$BRANCH..HEAD
+endef
