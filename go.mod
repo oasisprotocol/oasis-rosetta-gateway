81e17266f0e7707627770d9ff5e845d70bc3e67e
Warren He <w@oasislabs.com>
2021-12-20 16:40:21 -0800

    check-prep: use new PrefundedAccounts

--- go.mod
+++ go.mod
@@ -7,11 +7,7 @@ replace github.com/tendermint/tendermint => github.com/oasisprotocol/tendermint 
 require (
 	github.com/coinbase/rosetta-cli v0.7.3
 	github.com/coinbase/rosetta-sdk-go v0.7.2
-	// hacks: check-prep reaches directly into rosetta-cli storage to see what account it wants to use
-	// in order to set that account up with some funds for testing
-	github.com/dgraph-io/badger v1.6.2
 	github.com/oasisprotocol/ed25519 v0.0.0-20210127160119-f7017427c1ea
 	github.com/oasisprotocol/oasis-core/go v0.2103.7
-	github.com/vmihailenco/msgpack/v5 v5.3.5
 	google.golang.org/grpc v1.41.0
 )
--- tests/check-prep/main.go
+++ tests/check-prep/main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"bytes"
+	"encoding/hex"
 	"io/ioutil"
-	"os"
-	"path"
 
 	"github.com/coinbase/rosetta-cli/configuration"
-	"github.com/coinbase/rosetta-sdk-go/keys"
+	"github.com/coinbase/rosetta-sdk-go/storage/modules"
 	"github.com/coinbase/rosetta-sdk-go/types"
-	"github.com/dgraph-io/badger"
 	"github.com/oasisprotocol/oasis-core/go/staking/api"
-	"github.com/vmihailenco/msgpack/v5"
 
 	"github.com/oasisprotocol/oasis-core-rosetta-gateway/services"
 	"github.com/oasisprotocol/oasis-core-rosetta-gateway/tests/common"
 )
-
-func storageEncode(v interface{}) []byte {
-	var buf bytes.Buffer
-	enc := msgpack.GetEncoder()
-	enc.Reset(&buf)
-	enc.SetCustomStructTag("json")
-	if err := enc.Encode(v); err != nil {
-		panic(err)
-	}
-	msgpack.PutEncoder(enc)
-	return buf.Bytes()
-}
 
 func getRosettaConfig(ni *types.NetworkIdentifier) *configuration.Configuration {
 	// Create a configuration file for the local testnet.
@@ -37,9 +21,22 @@ func getRosettaConfig(ni *types.NetworkIdentifier) *configuration.Configuration 
 
 	config.DataDirectory = "/tmp/rosetta-cli-oasistests"
 
-	config.Construction.Currency = services.OasisCurrency
-	config.Construction.MaximumFee = "0"
-	config.Construction.CurveType = types.Edwards25519
+	testEntityAddress, testEntityKeyPair := common.TestEntity()
+	config.Construction = &configuration.ConstructionConfiguration{
+		PrefundedAccounts: []*modules.PrefundedAccount{
+			{
+				PrivateKeyHex: hex.EncodeToString(testEntityKeyPair.PrivateKey),
+				AccountIdentifier: &types.AccountIdentifier{
+					Address:    testEntityAddress,
+					SubAccount: nil,
+					Metadata:   nil,
+				},
+				CurveType: types.Edwards25519,
+				Currency:  services.OasisCurrency,
+			},
+		},
+	}
+
 	config.Construction.Scenario = []*types.Operation{
 		{
 			OperationIdentifier: &types.OperationIdentifier{
@@ -105,51 +102,6 @@ func main() {
 
 	config := getRosettaConfig(ni)
 	if err = ioutil.WriteFile("rosetta-cli-config.json", []byte(common.DumpJSON(config)), 0o600); err != nil {
-		panic(err)
-	}
-
-	// Create an account for construction tests.
-	testEntityAddress, testEntityKeyPair := common.TestEntity()
-
-	constructionStorePath := path.Join(config.DataDirectory, "check-construction", types.Hash(config.Network))
-	if err = os.MkdirAll(constructionStorePath, 0o777); err != nil {
-		panic(err)
-	}
-	db, err := badger.Open(badger.DefaultOptions(constructionStorePath))
-	if err != nil {
-		panic(err)
-	}
-	if err := db.Update(func(txn *badger.Txn) error {
-		if err := txn.Set([]byte("key/"+testEntityAddress), storageEncode(&struct {
-			Address string        `json:"address"`
-			KeyPair *keys.KeyPair `json:"keypair"`
-		}{
-			Address: testEntityAddress,
-			KeyPair: testEntityKeyPair,
-		})); err != nil {
-			panic(err)
-		}
-		testEntityAccountIdentifier := types.AccountIdentifier{Address: testEntityAddress}
-		if err := txn.Set(
-			[]byte("balance/"+types.Hash(&testEntityAccountIdentifier)+"/"+types.Hash(services.OasisCurrency)),
-			storageEncode(
-				&struct {
-					Account *types.AccountIdentifier `json:"account"`
-					Amount  *types.Amount            `json:"amount"`
-					Block   *types.BlockIdentifier   `json:"block"`
-				}{
-					Account: &testEntityAccountIdentifier,
-					Amount:  common.TestEntityAmount,
-				},
-			),
-		); err != nil {
-			panic(err)
-		}
-		return nil
-	}); err != nil {
-		panic(err)
-	}
-	if err := db.Close(); err != nil {
 		panic(err)
 	}
 }
